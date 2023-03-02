830a52458bfde158469ea4e33a3dd50d08a67669
Yannik Hodel <yannik.hodel@gmail.com>
2023-03-02 16:21:48 +0100

    Implement Texture

--- GhostOfTsushima-Foliage/src/Util/Texture.cpp
+++ GhostOfTsushima-Foliage/src/Util/Texture.cpp
@@ -1,2 +1,76 @@
 #include "pch.h"
 #include "Texture.h"
+
+#include "stb_image.h"
+
+Texture::Texture(std::string&& path, bool flipVertically)
+    : m_Paths({ path }), m_TextureType(TextureType::TEXTURE_2D)
+{
+    stbi_set_flip_vertically_on_load(flipVertically);
+
+	glGenTextures(1, &m_TextureId);
+	glBindTexture(m_TextureType, m_TextureId);
+
+    glTexParameteri(m_TextureType, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(m_TextureType, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(m_TextureType, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(m_TextureType, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(m_Paths[0].c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        int format = nrChannels == 3 ? GL_RGB : GL_RGBA;
+        glTexImage2D(m_TextureType, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(m_TextureType);
+    }
+    else
+    {
+        std::cout << "Failed to load texture at path: " << m_Paths[0] << std::endl;
+    }
+    stbi_image_free(data);
+    glBindTexture(m_TextureType, 0);
+}
+
+Texture::Texture(std::vector<std::string> paths, bool flipVertically)
+    : m_Paths(paths), m_TextureType(TextureType::TEXTURE_CUBEMAP)
+{
+    stbi_set_flip_vertically_on_load(flipVertically);
+
+    glGenTextures(1, &m_TextureId);
+    glBindTexture(m_TextureType, m_TextureId);
+
+    glTexParameteri(m_TextureType, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(m_TextureType, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(m_TextureType, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(m_TextureType, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < paths.size(); i++)
+    {
+        unsigned char* data = stbi_load(paths[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            int format = nrChannels == 3 ? GL_RGB : GL_RGBA;
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Failed to load texture at path: " << paths[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+}
+
+Texture::~Texture()
+{
+    glDeleteTextures(1, &m_TextureId);
+}
+
+void Texture::ActivateForSlot(uint32_t slot)
+{
+	assert(slot <= 32);
+	glActiveTexture(GL_TEXTURE0 + slot);
+	glBindTexture(m_TextureType, m_TextureId);
+}
--- GhostOfTsushima-Foliage/src/Util/Texture.h
+++ GhostOfTsushima-Foliage/src/Util/Texture.h
@@ -1,10 +1,25 @@
 #pragma once
 
+enum TextureType
+{
+	TEXTURE_2D = GL_TEXTURE_2D,
+	TEXTURE_CUBEMAP = GL_TEXTURE_CUBE_MAP
+};
+
 class Texture
 {
 public:
-	Texture(std::string&& path) : m_Path(path) {}
+	Texture(std::string&& path, bool flipVertically = false);
+	Texture(std::vector<std::string> paths, bool flipVertically = false);
+	~Texture();
+
+	void ActivateForSlot(uint32_t slot);
+
+	unsigned int GetTextureId() { return m_TextureId; }
+	unsigned int GetTextureType() { return m_TextureType; }
 
 private:
-	std::string m_Path;
+	std::vector<std::string> m_Paths;
+	unsigned int m_TextureId;
+	TextureType m_TextureType;
 };
