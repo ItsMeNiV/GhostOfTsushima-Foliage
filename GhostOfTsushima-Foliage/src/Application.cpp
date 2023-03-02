856808a89c16469944412b966ff661bad6a1d13a
Yannik Hodel <yannik.hodel@gmail.com>
2023-03-02 16:22:08 +0100

    Start implementation of main Application class

--- GhostOfTsushima-Foliage/src/Application.cpp
+++ GhostOfTsushima-Foliage/src/Application.cpp
@@ -2,8 +2,31 @@
 #include "Application.h"
 
 Application::Application()
+    : m_Camera(CreateRef<Camera>(glm::vec3(0.0f, 2.0f, 3.0f), m_WindowWidth, m_WindowHeight)),
+      m_CameraController(CreateRef<CameraControllerFirstPerson>(m_Camera.get(), 3.0f, 0.1f))
 {
     initOpenGLWithGLFW(m_WindowTitle, m_WindowWidth, m_WindowHeight);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glfwSwapInterval(0);
+    disableCursor();
+    glfwSetWindowUserPointer(window, reinterpret_cast<void*>(this));
+
+    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xPos, double yPos) {
+        Application* app = reinterpret_cast<Application*>(glfwGetWindowUserPointer(window));
+        app->mouseCallback(window, xPos, yPos);
+    });
+    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        Application* app = reinterpret_cast<Application*>(glfwGetWindowUserPointer(window));
+        app->keyCallback(window, key, scancode, action, mods);
+    });
+    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+        Application* app = reinterpret_cast<Application*>(glfwGetWindowUserPointer(window));
+        app->GetCamera()->UpdateWindowSize(width, height);
+        });
+
     loadWorld(0);
 }
 
@@ -15,8 +38,16 @@ Application::~Application()
 
 void Application::Run()
 {
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
     mainLoop
     {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window, deltaTime);
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -35,6 +66,67 @@ void Application::loadWorld(uint32_t worldNumber)
     default:
         std::cout << "World with the given number " << std::to_string(worldNumber) << " not available!" << std::endl;
         break;
+    }
+}
+
+void Application::keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        m_IsFocused = !m_IsFocused;
+        if (m_IsFocused)
+            disableCursor();
+        else
+            enableCursor();
+        m_FirstMouse = true;
+    }
+}
+
+void Application::mouseCallback(GLFWwindow* window, double xPos, double yPos)
+{
+    if (m_IsFocused)
+    {
+        float xpos = static_cast<float>(xPos);
+        float ypos = static_cast<float>(yPos);
+
+        if (m_FirstMouse)
+        {
+            m_LastX = xpos;
+            m_LastY = ypos;
+            m_FirstMouse = false;
+        }
+
+        float xOffset = xpos - m_LastX;
+        float yOffset = m_LastY - ypos;
+        m_LastX = xpos;
+        m_LastY = ypos;
+
+        m_CameraController->ProcessMouseMovement(xOffset, yOffset);
+    }
+}
+
+void Application::processInput(GLFWwindow* window, float deltaTime)
+{
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL))
+        return;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        m_CameraController->ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        m_CameraController->ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        m_CameraController->ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        m_CameraController->ProcessKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS && !m_Sprinting)
+    {
+        m_CameraController->SetSpeed(8.0f);
+        m_Sprinting = true;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE && m_Sprinting)
+    {
+        m_CameraController->SetSpeed(3.0f);
+        m_Sprinting = false;
     }
 }
 
--- GhostOfTsushima-Foliage/src/Application.h
+++ GhostOfTsushima-Foliage/src/Application.h
@@ -7,14 +7,27 @@ public:
 	Application();
 	~Application();
 
+	Ref<Camera> GetCamera() { return m_Camera; }
+
 	void Run();
 
 private:
 	const char* m_WindowTitle = "Ghost of Tsushima Foliage Rendering";
 	const uint32_t m_WindowWidth = 1600;
 	const uint32_t m_WindowHeight = 900;
+	bool m_Sprinting = false;
+	bool m_IsFocused = true;
+	bool m_FirstMouse = true;
+	float m_LastX = m_WindowWidth / 2.0f;
+	float m_LastY = m_WindowHeight / 2.0f;
 
 	Ref<World> m_World;
+	Ref<Camera> m_Camera;
+	Ref<CameraControllerFirstPerson> m_CameraController;
 
 	void loadWorld(uint32_t worldNumber);
+
+	void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	void mouseCallback(GLFWwindow* window, double xPos, double yPos);
+	void processInput(GLFWwindow* window, float deltaTime);
 };
